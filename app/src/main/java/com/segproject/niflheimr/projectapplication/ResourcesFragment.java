15442bf4836db1db324290799ca260c40efe26f0
Xiuzhu Li <xli262@uottawa.ca>
2017-12-03 10:35:26 -0500

    Fragments failed again, pls send some help

--- app/src/main/java/com/segproject/niflheimr/projectapplication/ResourcesFragment.java
+++ app/src/main/java/com/segproject/niflheimr/projectapplication/ResourcesFragment.java
@@ -1,10 +1,11 @@
 package com.segproject.niflheimr.projectapplication;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 
 /**
  * Created by Niflheimr on 2017-12-01.
@@ -13,7 +14,7 @@ import android.view.ViewGroup;
 public class ResourcesFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.people_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.resources_fragment, container, false);
 
         return rootView;
     }

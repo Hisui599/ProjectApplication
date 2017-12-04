119bdd5ccc78e8680822f60e36817a4d64eeaf75
Xiuzhu Li <xli262@uottawa.ca>
2017-12-03 19:22:57 -0500

    create task & group activity added

---
+++ app/src/main/java/com/segproject/niflheimr/projectapplication/CreateGroupActivity.java
@@ -0,0 +1,18 @@
+package com.segproject.niflheimr.projectapplication;
+
+/**
+ * Created by Niflheimr on 2017-12-03.
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+public class CreateGroupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_group);
+    }
+}
